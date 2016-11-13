5f44a032c8f259ba9c372709eacf16bf50b380d8
Kevin Avrillon <kavrillon@leadformance.com>
2016-11-13 18:54:02 +0100

    Release 1.0.22 - 2016-11-13T17:53:22.329Z

--- sw.js
+++ sw.js
@@ -1,54 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-var Config = {
-    name: 'fo-monitoring',
-    shellCacheName: 'fo-monitoring',
-    dataCacheName: 'fo-monitoring-data',
-    version: 1,
-    debug: true,
-    trello: {
-        key: 'd54e7add79eb871a615dcdb47033a99b',
-        connectUrl: 'https://trello.com/1/authorize?name=fo-monitoring&scope=read&expiration=30days&response_type=token',
-        url: 'https://api.trello.com/1/boards/577130dfed8fabf757eddc60/lists',
-        filters: '?cards=open&card_fields=name,labels&fields=name,desc'
-    },
-    stores: {
-        TestModel: {
-            properties: {
-                autoIncrement: true,
-                keyPath: 'url'
-            },
-            indexes: {
-                time: { unique: true }
-            }
-        },
-        AppModel: {
-            deleteOnUpgrade: true,
-            properties: {
-                autoIncrement: true
-            }
-        }
-    }
-};
-
-exports.default = Config;
-
-},{}],2:[function(require,module,exports){
-'use strict';
-
-var _Config = require('./scripts/config/Config');
-
-var _Config2 = _interopRequireDefault(_Config);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var cacheName = _Config2.default.shellCacheName + '-v1';
-var dataCacheName = _Config2.default.dataCacheName + '-v1';
-var filesToCache = ['', 'index.html', 'app-d16be5b46c.js', 'app-65ad6bfb9a.css', 'fonts/303098_0_0.eot', 'fonts/303098_0_0.ttf', 'fonts/303098_0_0.woff', 'fonts/303098_0_0.woff2', 'fonts/303098_1_0.eot', 'fonts/303098_1_0.ttf', 'fonts/303098_1_0.woff', 'fonts/303098_1_0.woff2', 'fonts/303098_2_0.eot', 'fonts/303098_2_0.ttf', 'fonts/303098_2_0.woff', 'fonts/303098_2_0.woff2', 'fonts/303098_3_0.eot', 'fonts/303098_3_0.ttf', 'fonts/303098_3_0.woff', 'fonts/303098_3_0.woff2', 'fonts/303098_4_0.eot', 'fonts/303098_4_0.ttf', 'fonts/303098_4_0.woff', 'fonts/303098_4_0.woff2', 'fonts/303098_5_0.eot', 'fonts/303098_5_0.ttf', 'fonts/303098_5_0.woff', 'fonts/303098_5_0.woff2', 'fonts/303098_6_0.eot', 'fonts/303098_6_0.ttf', 'fonts/303098_6_0.woff', 'fonts/303098_6_0.woff2', 'fonts/303098_7_0.eot', 'fonts/303098_7_0.ttf', 'fonts/303098_7_0.woff', 'fonts/303098_7_0.woff2', 'fonts/303098_8_0.eot', 'fonts/303098_8_0.ttf', 'fonts/303098_8_0.woff', 'fonts/303098_8_0.woff2', 'fonts/303098_9_0.eot', 'fonts/303098_9_0.ttf', 'fonts/303098_9_0.woff', 'fonts/303098_9_0.woff2', 'fonts/303098_A_0.eot', 'fonts/303098_A_0.ttf', 'fonts/303098_A_0.woff', 'fonts/303098_A_0.woff2', 'fonts/303098_B_0.eot', 'fonts/303098_B_0.ttf', 'fonts/303098_B_0.woff', 'fonts/303098_B_0.woff2', 'images/icon-144x144.png', 'images/icon-152x152.png', 'images/icon-192x192.png', 'images/icon-384x384.png', 'images/favicon.ico', 'images/ic_add_white_24px.svg', 'images/ic_info_outline_24px.svg', 'images/ic_menu_24px.svg', 'images/ic_refresh_white_24px.svg', 'images/side-nav-bg@2x.jpg'];
+var cacheName = 'fo-monitoring-v1';
+var dataCacheName = 'fo-monitoring-data-v1';
+var filesToCache = ['', 'index.html', 'app-94648e7adf.js', 'app-c23d141d1f.css', 'fonts/303098_0_0.eot', 'fonts/303098_0_0.ttf', 'fonts/303098_0_0.woff', 'fonts/303098_0_0.woff2', 'fonts/303098_1_0.eot', 'fonts/303098_1_0.ttf', 'fonts/303098_1_0.woff', 'fonts/303098_1_0.woff2', 'fonts/303098_2_0.eot', 'fonts/303098_2_0.ttf', 'fonts/303098_2_0.woff', 'fonts/303098_2_0.woff2', 'fonts/303098_3_0.eot', 'fonts/303098_3_0.ttf', 'fonts/303098_3_0.woff', 'fonts/303098_3_0.woff2', 'fonts/303098_4_0.eot', 'fonts/303098_4_0.ttf', 'fonts/303098_4_0.woff', 'fonts/303098_4_0.woff2', 'fonts/303098_5_0.eot', 'fonts/303098_5_0.ttf', 'fonts/303098_5_0.woff', 'fonts/303098_5_0.woff2', 'fonts/303098_6_0.eot', 'fonts/303098_6_0.ttf', 'fonts/303098_6_0.woff', 'fonts/303098_6_0.woff2', 'fonts/303098_7_0.eot', 'fonts/303098_7_0.ttf', 'fonts/303098_7_0.woff', 'fonts/303098_7_0.woff2', 'fonts/303098_8_0.eot', 'fonts/303098_8_0.ttf', 'fonts/303098_8_0.woff', 'fonts/303098_8_0.woff2', 'fonts/303098_9_0.eot', 'fonts/303098_9_0.ttf', 'fonts/303098_9_0.woff', 'fonts/303098_9_0.woff2', 'fonts/303098_A_0.eot', 'fonts/303098_A_0.ttf', 'fonts/303098_A_0.woff', 'fonts/303098_A_0.woff2', 'fonts/303098_B_0.eot', 'fonts/303098_B_0.ttf', 'fonts/303098_B_0.woff', 'fonts/303098_B_0.woff2', 'images/icon-144x144.png', 'images/icon-152x152.png', 'images/icon-192x192.png', 'images/icon-384x384.png', 'images/favicon.ico', 'images/ic_add_white_24px.svg', 'images/ic_info_outline_24px.svg', 'images/ic_menu_24px.svg', 'images/ic_refresh_white_24px.svg', 'images/side-nav-bg@2x.jpg'];
 
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
@@ -73,11 +28,7 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
     console.log('[Service Worker] Fetch', e.request.url);
-
-    // var data = '?cards=open&card_fields=name,labels&fields=name,desc&key=' + Trello.key() + '&token=' + Trello.token();
-    // var url = 'https://api.trello.com/1/boards/577130dfed8fabf757eddc60/lists' + data;
-
-    var dataUrl = '' + _Config2.default.trello.url + _Config2.default.trello.filters;
+    var dataUrl = 'https://api.trello.com/1/';
 
     if (e.request.url.indexOf(dataUrl) > -1) {
         e.respondWith(caches.open(dataCacheName).then(function (cache) {
@@ -93,7 +44,7 @@ self.addEventListener('fetch', function (e) {
     }
 });
 
-},{"./scripts/config/Config":1}]},{},[2])
+},{}]},{},[1])
 
 
 //# sourceMappingURL=sw.js.map
